ed28dea3ad70e4a5d5c8bdcc58a4d8f8b1100c7c
aurelianania <nania.aurelia@gmail.com>
2026-07-19 13:43:14 +0700

    Update exercise2_dict.py

--- 01-python-recall/exercises/exercise2_dict.py
+++ 01-python-recall/exercises/exercise2_dict.py
@@ -1,1 +1,22 @@
+#Buat sebuah program yang menghitung frekuensi kemunculan kata dalam sebuah teks pendek (string).
+#Simpan hasilnya dalam bentuk Dictionary { "kata": jumlah_kemunculan }
+def hitung_kata(teks):
+    frekuensi={}
 
+    kumpulan_kata= teks.split()
+
+    for kata in kumpulan_kata:
+        kata_bersih= kata.lower()
+
+        if kata_bersih in frekuensi:
+            frekuensi[kata_bersih] += 1
+        
+        else:
+            frekuensi[kata_bersih] = 1
+    
+    return frekuensi
+
+kalimat_contoh= "Piksel gambar itu berwarna merah dan piksel ini berwarna biru"
+hasil_hitung= hitung_kata(kalimat_contoh)
+
+print(hasil_hitung)
